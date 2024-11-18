61aa58266afbff258273fd49eb7e925a3d73b853
lmkn5342@gmail.com <lmkn5342@gmail.com>
2024-11-18 20:03:26 +0900

    `feat` : make restaurant controller

--- src/main/java/com/example/banggusuk_michelin/controller/RestaurantController.java
+++ src/main/java/com/example/banggusuk_michelin/controller/RestaurantController.java
@@ -1,7 +1,29 @@
 package com.example.banggusuk_michelin.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.banggusuk_michelin.apiFormat.ApiResponse;
+import com.example.banggusuk_michelin.dto.RestaurantCreationDto;
+import com.example.banggusuk_michelin.entity.User;
+import com.example.banggusuk_michelin.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Map;
+
+@RestController
+@RequestMapping("/restaurant")
+@RequiredArgsConstructor
 public class RestaurantController {
+    private final RestaurantService restaurantService;
+
+    @PostMapping()
+    public ApiResponse<Map<String, Object>> createRestaurant(@RequestBody RestaurantCreationDto restaurantCreationDto,
+                                                             @AuthenticationPrincipal User user){
+        return ApiResponse.success(Map.of());
+    }
+
+    @GetMapping()
+    public ApiResponse<Map<String, Object>> viewRestaurant(@RequestParam int rating) {
+        return ApiResponse.success(Map.of());
+    }
 }
