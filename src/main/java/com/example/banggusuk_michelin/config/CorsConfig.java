c515c886650538f59bc6715bdda82a27c533ccbc
lmkn5342@gmail.com <lmkn5342@gmail.com>
2024-12-15 14:03:53 +0900

    Update CorsConfig.java

--- src/main/java/com/example/banggusuk_michelin/config/CorsConfig.java
+++ src/main/java/com/example/banggusuk_michelin/config/CorsConfig.java
@@ -14,10 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("*")); // 모든 도메인 허용
+        configuration.setAllowedOrigins(List.of("*")); // 모든 도메인 허용
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE")); // 모든 메소드 허용
         configuration.setAllowedHeaders(List.of("*")); // 모든 헤더 허용
-        configuration.setAllowCredentials(true); // 자격 증명 허용
+//        configuration.setAllowCredentials(true); // 자격 증명 허용
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 적용
